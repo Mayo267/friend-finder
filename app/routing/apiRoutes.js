9c612c3d20cdd0f125da3b4ab3b72ae73a5944b8
Andrew Mayo <amayo0925@gmail.com>
2019-02-19 22:07:01 -0500

    working server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,13 @@
+var friendData = require("../data/friends");
 
+module.exports = function(app){
+    app.get("/api/friends", function(req, res){
+        res.json(friendData);
+    });
+
+    // app.post("/api/friends", function(req, res){
+    //     //If results match close to input, push to...
+    //     //something.push(req.body);
+    //     //else....
+    // });
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,19 @@
+//=========================
+// DEPENDENCIES
+//=========================
 
+var path = require("path");
+
+//=========================
+//  ROUTING
+//=========================
+
+module.exports = function(app){
+    app.get("/home", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+};
