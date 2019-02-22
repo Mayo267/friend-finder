d836a886972fabe3df2ac45bd38265051a278f40
Andrew Mayo <amayo0925@gmail.com>
2019-02-22 16:37:13 -0500

    something

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.get("/home", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// app.get("/survey", function(req, res){
+//     res.sendFile(path.join(__dirname, "survey.html"));
+// });
+
+
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
