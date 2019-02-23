bbbb79f47971e3b08daa438dcd61f2112ebe3374
Andrew Mayo <amayo0925@gmail.com>
2019-02-23 10:19:16 -0500

    server

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
 
+app.use(routes);
+
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 });
